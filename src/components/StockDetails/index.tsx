8052e70fc3a362abee49442e3d8cc275fdd2f633
Franh01 <fran_lopez9@live.com>
2025-01-31 22:27:47 -0300

    feat: :sparkles: see all transactions by stock

--- src/components/StockDetails/index.tsx
+++ src/components/StockDetails/index.tsx
@@ -1,7 +1,27 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useParams } from "react-router-dom"
+import {
+  selectInitialStatus,
+  selectTransactions,
+} from "@components/Transactions/transactionSlice"
+
+import { Box } from "@mui/material"
+import DefaultTable from "@components/DefaultTable/DefaultTable"
+import { useAppSelector } from "@app/hooks"
 
 const StockDetails = () => {
-  return <NavLink to={`/`}>HOME</NavLink>
+  const { symbol } = useParams<{ symbol: string }>()
+  const transactions = useAppSelector(selectTransactions)
+  const status = useAppSelector(selectInitialStatus)
+
+  return (
+    <Box>
+      <NavLink to={`/`}>HOME</NavLink>
+      <DefaultTable
+        status={status}
+        transactions={transactions.filter(t => t.ticker === symbol)}
+      />
+    </Box>
+  )
 }
 
 export default StockDetails
