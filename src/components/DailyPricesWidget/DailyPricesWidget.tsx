15aec17dae8e130e3ab376dd3f945b81542c4ea3
Franh01 <fran_lopez9@live.com>
2025-02-03 23:15:42 -0300

    feat: :sparkles: add daily stock prices in layout

---
+++ src/components/DailyPricesWidget/DailyPricesWidget.tsx
@@ -0,0 +1,48 @@
+import { Box, Typography } from "@mui/material"
+
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown"
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp"
+import type { IStockInformation } from "@interfaces/index"
+
+interface IDailyPricesWidgetProps {
+  data: IStockInformation
+}
+
+const DailyPricesWidget = ({ data }: IDailyPricesWidgetProps) => {
+  console.log(data, "data")
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        mr: 6,
+        color: "var(--dark-contrast)",
+      }}
+    >
+      <Typography mr={"10px"} fontSize={"14px"}>
+        {data.symbol}
+      </Typography>
+      <Typography fontSize={"14px"} fontWeight={"500"}>
+        {data.currentPrice}
+      </Typography>
+      {data.dailyChange > 0 ? (
+        <ArrowDropUpIcon sx={{ color: "var(--higher-color)" }} />
+      ) : (
+        <ArrowDropDownIcon sx={{ color: "var(--lower-color)" }} />
+      )}
+      <Typography
+        sx={{
+          fontWeight: "500",
+          fontSize: "14px",
+          color:
+            data.dailyChange > 0 ? "var(--higher-color)" : "var(--lower-color)",
+        }}
+      >
+        {data.dailyChangePercent.toFixed(2)}%
+      </Typography>
+    </Box>
+  )
+}
+
+export default DailyPricesWidget
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,15 +1,46 @@
-import { AppBar, Box, Toolbar, Typography } from "@mui/material"
+import { Box, Typography } from "@mui/material"
+import {
+  selectStatus,
+  selectStockInformations,
+} from "@components/StockInformation/stockInformationSlice"
 
+import DailyPricesWidget from "@components/DailyPricesWidget/DailyPricesWidget"
 import { Outlet } from "react-router-dom"
+import { useAppSelector } from "@app/hooks"
 
 const Layout = () => {
+  const allStockInformationStatus = useAppSelector(selectStatus)
+  const allStockInformation = useAppSelector(selectStockInformations)
+
+  const MapStockInformation = () => {
+    // Acumula los elementos JSX en un array
+    const stockInfoElements = Object.entries(allStockInformation).map(
+      ([key, value]) => <DailyPricesWidget data={value} key={key} />,
+    )
+    // Retorna el array de elementos JSX
+    return stockInfoElements
+  }
+
   return (
     <Box>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">Investment Records</Typography>
-        </Toolbar>
-      </AppBar>
+      <Box
+        sx={{
+          width: "100%",
+          height: "35px",
+          position: "fixed",
+          top: 0,
+          background: "var(--dark-background)",
+          display: "flex",
+          padding: "0 20px",
+        }}
+      >
+        {allStockInformationStatus === "loading" ? (
+          <Typography>Loading...</Typography>
+        ) : (
+          <MapStockInformation />
+        )}
+      </Box>
+
       <Box component="main" sx={{ p: 3 }}>
         <Outlet />
       </Box>
